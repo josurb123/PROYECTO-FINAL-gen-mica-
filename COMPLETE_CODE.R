2d682178907af9072b706dfd2d6bd991d458af83
leydiuaq <lrobles15@gmail.com>
2026-05-16 01:49:56 -0600

    cambie los nombres de las variables ya que, al ser un codigo tan largo, considere que el m, n, j, k no daban informacion valiosa de que era lo que se estaba trabajando y me hacia confundirme. Continue el filtrado con threshold como lo pidio josue y comence un ciclo for que elimine los nodos y genere los objetos de cada una, estoy algo perdida asi que voy a seguir intentando, de lo contrario lo hare de forma manual. Genere una lista de mis archivos y una lista vacia que se rellenara con el ciclo for, un objeto para que no escriba en los datos originales y que a partir del degree vaya filtrando, lo cual tiene sentido porque si es cero, simplemente no esta conectado. lo que me falta es que conforme vaya corriendo, asigne el titulo e imprima la red. Sin embargo, me atore un poco, asi que lo dejo por el momento.

--- COMPLETE_CODE.R
+++ COMPLETE_CODE.R
@@ -1,216 +1,379 @@
+
 #AQUI SE VA A HACER TODO EL CÓDIGO EL CUAL SE VA A PONER EN EL R MARKDOWN
 #Importar matrices a objetos de database de R
-matriz_todos<-read.csv("C:/Users/us/Documents/GENOMICA/PROYECTO FINAL (genómica)/RAW DATA/B-Co-Occurrence All.csv")
-View(matriz_todos)
-row.names(matriz_todos)<-matriz_todos[,1]
-matriz_todos<-matriz_todos[,-1]
-colnames(matriz_todos)<-matriz_todos[1,]
-matriz_todos<-matriz_todos[-1,]
 
-matriz_C<-read.csv("C:/Users/us/Documents/GENOMICA/PROYECTO FINAL (genómica)/RAW DATA/C- Co-Occurrence Healthy & Lean.csv")
-View(matriz_C)
-row.names(matriz_C)<-matriz_C[,1]
-matriz_C<-matriz_C[,-1]
-colnames(matriz_C)<-matriz_C[1,]
-matriz_C<-matriz_C[-1,]
+todos<-read.csv("RAW DATA/B-Co-Occurrence All.csv")
+View(todos)
+row.names(todos)<-todos[,1]
+todos<-todos[,-1]
+colnames(todos)<-todos[1,]
+todos<-todos[-1,]
 
-matriz_D<-read.csv("C:/Users/us/Documents/GENOMICA/PROYECTO FINAL (genómica)/RAW DATA/D-Co-Occurrence Healthy & Obese.csv")
-View(matriz_D)
-row.names(matriz_D)<-matriz_D[,1]
-matriz_D<-matriz_D[,-1]
-colnames(matriz_D)<-matriz_D[1,]
-matriz_D<-matriz_D[-1,]
+#Puse nombres que podamos identificar de mejor manera ya que los archivos
+#tambien estan clasificados, asi que siento que genera mucha confusion
+#poner matriz_a-j (por ejemplo h_lean "healthy & lean")
 
-matriz_E<-read.csv("C:/Users/us/Documents/GENOMICA/PROYECTO FINAL (genómica)/RAW DATA/E-Co-Occurrence IBD & Lean.csv")
-View(matriz_E)
-row.names(matriz_E)<-matriz_E[,1]
-matriz_E<-matriz_E[,-1]
-colnames(matriz_E)<-matriz_E[1,]
-matriz_E<-matriz_E[-1,]
+h_lean<-read.csv("RAW DATA/C- Co-Occurrence Healthy & Lean.csv")
+View(h_lean)
+row.names(h_lean)<-h_lean[,1]
+h_lean<-h_lean[,-1]
+colnames(h_lean)<-h_lean[1,]
+h_lean<-h_lean[-1,]
 
-matriz_F<-read.csv("C:/Users/us/Documents/GENOMICA/PROYECTO FINAL (genómica)/RAW DATA/F-Co-Occurrence IBD & Obese.csv")
-View(matriz_F)
-row.names(matriz_F)<-matriz_F[,1]
-matriz_F<-matriz_F[,-1]
-colnames(matriz_F)<-matriz_F[1,]
-matriz_F<-matriz_F[-1,]
+h_obese<-read.csv("RAW DATA/D-Co-Occurrence Healthy & Obese.csv")
+View(h_obese)
+row.names(h_obese)<-h_obese[,1]
+h_obese<-h_obese[,-1]
+colnames(h_obese)<-h_obese[1,]
+h_obese<-h_obese[-1,]
 
-matriz_G<-read.csv("C:/Users/us/Documents/GENOMICA/PROYECTO FINAL (genómica)/RAW DATA/G-Co-Occurrence Danish.csv")
-View(matriz_G)
-row.names(matriz_G)<-matriz_G[,1]
-matriz_G<-matriz_G[,-1]
-colnames(matriz_G)<-matriz_G[1,]
-matriz_G<-matriz_G[-1,]
+i_lean<-read.csv("RAW DATA/E-Co-Occurrence IBD & Lean.csv")
+View(i_lean)
+row.names(i_lean)<-i_lean[,1]
+i_lean<-i_lean[,-1]
+colnames(i_lean)<-i_lean[1,]
+i_lean<-i_lean[-1,]
 
-matriz_H<-read.csv("C:/Users/us/Documents/GENOMICA/PROYECTO FINAL (genómica)/RAW DATA/H-Co-Occurrence Spanish.csv")
-View(matriz_H)
-row.names(matriz_H)<-matriz_H[,1]
-matriz_H<-matriz_H[,-1]
-colnames(matriz_H)<-matriz_H[1,]
-matriz_H<-matriz_H[-1,]
+i_obese<-read.csv("RAW DATA/F-Co-Occurrence IBD & Obese.csv")
+View(i_obese)
+row.names(i_obese)<-i_obese[,1]
+i_obese<-i_obese[,-1]
+colnames(i_obese)<-i_obese[1,]
+i_obese<-i_obese[-1,]
 
-matriz_I<-read.csv("C:/Users/us/Documents/GENOMICA/PROYECTO FINAL (genómica)/RAW DATA/I-Co-Occurrence ET1.csv")
-View(matriz_I)
-row.names(matriz_I)<-matriz_I[,1]
-matriz_I<-matriz_I[,-1]
-colnames(matriz_I)<-matriz_I[1,]
-matriz_I<-matriz_I[-1,]
+danish<-read.csv("RAW DATA/G-Co-Occurrence Danish.csv")
+View(danish)
+row.names(danish)<-danish[,1]
+danish<-danish[,-1]
+colnames(danish)<-danish[1,]
+danish<-danish[-1,]
 
-matriz_K<-read.csv("C:/Users/us/Documents/GENOMICA/PROYECTO FINAL (genómica)/RAW DATA/K-Co-Occurrence ET2.csv")
-View(matriz_K)
-row.names(matriz_K)<-matriz_K[,1]
-matriz_K<-matriz_K[,-1]
-colnames(matriz_K)<-matriz_K[1,]
-matriz_K<-matriz_K[-1,]
+spanish<-read.csv("RAW DATA/H-Co-Occurrence Spanish.csv")
+View(spanish)
+row.names(spanish)<-spanish[,1]
+spanish<-spanish[,-1]
+colnames(spanish)<-spanish[1,]
+spanish<-spanish[-1,]
 
-matriz_L<-read.csv("C:/Users/us/Documents/GENOMICA/PROYECTO FINAL (genómica)/RAW DATA/L-Co-Occurrence ET3.csv")
-View(matriz_L)
-row.names(matriz_L)<-matriz_L[,1]
-matriz_L<-matriz_L[,-1]
-colnames(matriz_L)<-matriz_L[1,]
-matriz_L<-matriz_L[-1,]
+ET1<-read.csv("RAW DATA/I-Co-Occurrence ET1.csv")
+View(ET1)
+row.names(ET1)<-ET1[,1]
+ET1<-ET1[,-1]
+colnames(ET1)<-ET1[1,]
+ET1<-ET1[-1,]
+
+ET2<-read.csv("RAW DATA/K-Co-Occurrence ET2.csv")
+View(ET2)
+row.names(ET2)<-ET2[,1]
+ET2<-ET2[,-1]
+colnames(ET2)<-ET2[1,]
+ET2<-ET2[-1,]
+
+ET3<-read.csv("RAW DATA/L-Co-Occurrence ET3.csv")
+View(ET3)
+row.names(ET3)<-ET3[,1]
+ET3<-ET3[,-1]
+colnames(ET3)<-ET3[1,]
+ET3<-ET3[-1,]
 
 #hacer que R lea el contenido de la tabla como valores numericos
-matriz_todos[] <- lapply(matriz_todos, as.numeric)
-matriz_C[] <- lapply(matriz_C, as.numeric)
-matriz_D[] <- lapply(matriz_D, as.numeric)
-matriz_E[] <- lapply(matriz_E, as.numeric)
-matriz_F[] <- lapply(matriz_F, as.numeric)
-matriz_G[] <- lapply(matriz_G, as.numeric)
-matriz_H[] <- lapply(matriz_H, as.numeric)
-matriz_I[] <- lapply(matriz_I, as.numeric)
-matriz_K[] <- lapply(matriz_K, as.numeric)
-matriz_L[] <- lapply(matriz_L, as.numeric)
+todos[] <- lapply(todos, as.numeric)
+h_lean[] <- lapply(h_lean, as.numeric)
+h_obese[] <- lapply(h_obese, as.numeric)
+i_lean[] <- lapply(i_lean, as.numeric)
+i_obese[] <- lapply(i_obese, as.numeric)
+danish[] <- lapply(danish, as.numeric)
+spanish[] <- lapply(spanish, as.numeric)
+ET1[] <- lapply(ET1, as.numeric)
+ET2[] <- lapply(ET2, as.numeric)
+ET3[] <- lapply(ET3, as.numeric)
 
 #volver todos los dataframes en matrices
-mtodos<-as.matrix(matriz_todos)
-mC<-as.matrix(matriz_C)
-mD<-as.matrix(matriz_D)
-mE<-as.matrix(matriz_E)
-mF<-as.matrix(matriz_F)
-mG<-as.matrix(matriz_G)
-mH<-as.matrix(matriz_H)
-mI<-as.matrix(matriz_I)
-mK<-as.matrix(matriz_K)
-mL<-as.matrix(matriz_L)
+m_todos<-as.matrix(todos)
+mh_lean<-as.matrix(h_lean)
+mh_obese<-as.matrix(h_obese)
+mi_lean<-as.matrix(i_lean)
+mi_obese<-as.matrix(i_obese)
+m_danish<-as.matrix(danish)
+m_spanish<-as.matrix(spanish)
+m_ET1<-as.matrix(ET1)
+m_ET2<-as.matrix(ET2)
+m_ET3<-as.matrix(ET3)
 
 #Verificar que sean valores numéricos en la matriz
-class(mtodos)
-mode(mtodos)
+class(m_todos)
+mode(m_todos)
 
 #observar cuantas NA's hay y cambiarlas por 0, ya que son matrices de similitud
-sum(is.na(mtodos))
-sum(is.na(mC))
-sum(is.na(mD))
-sum(is.na(mE))
-sum(is.na(mF))
-sum(is.na(mG))
-sum(is.na(mH))
-sum(is.na(mI))
-sum(is.na(mK))
-sum(is.na(mL))
+sum(is.na(m_todos))
+sum(is.na(mh_lean))
+sum(is.na(mh_obese))
+sum(is.na(mi_lean))
+sum(is.na(mi_obese))
+sum(is.na(m_danish))
+sum(is.na(m_spanish))
+sum(is.na(m_ET1))
+sum(is.na(m_ET2))
+sum(is.na(m_ET3))
 
-mtodos[is.na(mtodos)] <- 0
-mtodos[is.na(mC)] <- 0
-mtodos[is.na(mD)] <- 0
-mtodos[is.na(mE)] <- 0
-mtodos[is.na(mF)] <- 0
-mtodos[is.na(mG)] <- 0
-mtodos[is.na(mH)] <- 0
-mtodos[is.na(mI)] <- 0
-mtodos[is.na(mK)] <- 0
-mtodos[is.na(mL)] <- 0
+m_todos[is.na(m_todos)] <- 0
+mh_lean[is.na(mh_lean)] <- 0
+mh_obese[is.na(mh_obese)] <- 0
+mi_lean[is.na(mi_lean)] <- 0
+mi_obese[is.na(mi_obese)] <- 0
+m_danish[is.na(m_danish)] <- 0
+m_spanish[is.na(m_spanish)] <- 0
+m_ET1[is.na(m_ET1)] <- 0
+m_ET2[is.na(m_ET2)] <- 0
+m_ET3[is.na(m_ET3)] <- 0
 
 #la matriz de todos tiene 2 filas extra vacias, esas las vamos a borrar
-dim(mtodos)
-rownames(mtodos)
-colnames(mtodos)
+dim(m_todos)
+rownames(m_todos)
+colnames(m_todos)
 
-mtodos <- mtodos[-c(155,156), ]
+m_todos <- m_todos[-c(155,156), ]
 
 #empezar a hacer las redes usando el paquete "igraph"
 library(igraph)
 
 g_todos <- graph_from_adjacency_matrix(
-  mtodos,
+  m_todos,
   mode = "undirected",
   weighted = TRUE,
   diag = FALSE
 )
 
-g_C <- graph_from_adjacency_matrix(
-  mC,
+gh_lean <- graph_from_adjacency_matrix(
+  mh_lean,
   mode = "undirected",
   weighted = TRUE,
   diag = FALSE
 )
 
-g_D<- graph_from_adjacency_matrix(
-  mD,
+gh_obese<- graph_from_adjacency_matrix(
+  mh_obese,
   mode = "undirected",
   weighted = TRUE,
   diag = FALSE
 )
 
-g_E<- graph_from_adjacency_matrix(
-  mE,
+gi_lean<- graph_from_adjacency_matrix(
+  mi_lean,
   mode = "undirected",
   weighted = TRUE,
   diag = FALSE
 )
 
-g_F<- graph_from_adjacency_matrix(
-  mF,
+gh_obese<- graph_from_adjacency_matrix(
+  mi_obese,
   mode = "undirected",
   weighted = TRUE,
   diag = FALSE
 )
 
-g_G<- graph_from_adjacency_matrix(
-  mG,
+g_danish<- graph_from_adjacency_matrix(
+  m_danish,
   mode = "undirected",
   weighted = TRUE,
   diag = FALSE
 )
 
-g_H<- graph_from_adjacency_matrix(
-  mH,
+g_spanish<- graph_from_adjacency_matrix(
+  m_spanish,
   mode = "undirected",
   weighted = TRUE,
   diag = FALSE
 )
 
-g_I<- graph_from_adjacency_matrix(
-  mI,
+g_ET1<- graph_from_adjacency_matrix(
+  m_ET1,
   mode = "undirected",
   weighted = TRUE,
   diag = FALSE
 )
 
-g_K<- graph_from_adjacency_matrix(
-  mK,
+g_ET2<- graph_from_adjacency_matrix(
+  m_ET2,
   mode = "undirected",
   weighted = TRUE,
   diag = FALSE
 )
 
-g_L<- graph_from_adjacency_matrix(
-  mL,
+g_ET3<- graph_from_adjacency_matrix(
+  m_ET3,
   mode = "undirected",
   weighted = TRUE,
   diag = FALSE
 )
 
-#hacer threshold para quedarnos con las conexiones fuertes y eliminar las debiles
+#THRESHOLD PARA QUEDAR CON CONEXIONES FUERTES Y ELIMINAR DEBILES
 
-valstodos <- mtodos[upper.tri(mtodos)]#para usar solo los valores de la mitad superior de la tabla, ya que como la tabla es simetrica contiene la misma info cada una de las mitades, osea para no duplicar información
-threshold <- mean(valstodos) + sd(valstodos)
-threshold
-mtodos_thr <- mtodos #hacer nuevo objeto con el threshold
-mtodos_thr[mtodos_thr < threshold] <- 0 #hacer que todos los valores que esten debajo del threshold sea igual a 0
+###--TODOS--###
+vs_todos <- m_todos[upper.tri(m_todos)]
+t_todos <- mean(vs_todos) + sd(vs_todos)
+m_todos_thr <- m_todos 
+m_todos_thr[m_todos_thr < t_todos] <- 0  #hacer que todos los valores que esten debajo del threshold sea igual a 0
 
-#hacer con el resto de matrices
+###--HEALTHY & LEAN/OBESE--###
+vs_hl<- mh_lean[upper.tri(mh_lean)]
+th_lean<- mean(vs_hl) + sd(vs_hl)
+mh_lean_thr<- mh_lean
+mh_lean_thr[mh_lean_thr < th_lean] <- 0
+
+vs_ho <- mh_obese[upper.tri(mh_obese)]
+th_obese <- mean(vs_ho) + sd(vs_ho)
+mh_obese_thr <- mh_obese
+mh_obese_thr[mh_obese_thr < th_obese] <- 0
+
+##--IBD & LEAN/OBESE--##
+vs_il <- mi_lean[upper.tri(mi_lean)]
+ti_lean<- mean(vs_il) + sd(vs_il)
+mi_lean_thr<- mi_lean
+mi_lean_thr[mi_lean_thr < ti_lean] <- 0
+
+vs_io <- mi_obese[upper.tri(mi_obese)]
+ti_obese <- mean(vs_io) + sd(vs_io)
+mi_obese_thr <- mi_obese
+mi_obese_thr[mi_obese_thr < ti_obese] <- 0
+
+##--POBLACION: DANISH/SPANISH--##
+vs_danish <- m_danish[upper.tri(m_danish)]
+t_danish <- mean(vs_danish) + sd(vs_danish)
+m_danish_thr <- m_danish
+m_danish_thr[m_danish_thr < t_danish] <- 0
+
+vs_spanish <- m_spanish[upper.tri(m_spanish)]
+t_spanish <- mean(vs_spanish) + sd(vs_spanish)
+m_spanish_thr <- m_spanish
+m_spanish_thr[m_spanish_thr < t_spanish] <- 0
+
+##--ET1/2/3--##
+vs_ET1 <- m_ET1[upper.tri(m_ET1)]
+t_ET1 <- mean(vs_ET1) + sd(vs_ET1)
+m_ET1_thr <- m_ET1
+m_ET1_thr[m_ET1_thr < t_ET1] <- 0
+
+vs_ET2 <- m_ET2[upper.tri(m_ET2)]
+t_ET2 <- mean(vs_ET2) + sd(vs_ET2)
+m_ET2_thr <- m_ET2
+m_ET2_thr[m_ET2_thr < t_ET2] <- 0
+
+vs_ET3 <- m_ET3[upper.tri(m_ET3)]
+t_ET3 <- mean(vs_ET3) + sd(vs_ET3)
+m_ET3_thr <- m_ET3
+m_ET3_thr[m_ET3_thr < t_ET3] <- 0
+
 #después volver a hacer los grafos de con igraph pero ahora con las matrices con threshold
+
+g_todos_thr   <- graph_from_adjacency_matrix(m_todos_thr, mode = "undirected",
+                                             weighted = TRUE)
+gh_lean_thr   <- graph_from_adjacency_matrix(mh_lean_thr, mode = "undirected",
+                                             weighted = TRUE)
+gh_obese_thr  <- graph_from_adjacency_matrix(mh_obese_thr, mode = "undirected",
+                                             weighted = TRUE)
+gi_lean_thr   <- graph_from_adjacency_matrix(mi_lean_thr, mode = "undirected",
+                                             weighted = TRUE)
+gi_obese_thr  <- graph_from_adjacency_matrix(mi_obese_thr, mode = "undirected",
+                                             weighted = TRUE)
+g_danish_thr  <- graph_from_adjacency_matrix(m_danish_thr, mode = "undirected",
+                                             weighted = TRUE)
+g_spanish_thr <- graph_from_adjacency_matrix(m_spanish_thr, mode = "undirected",
+                                             weighted = TRUE)
+g_ET1_thr     <- graph_from_adjacency_matrix(m_ET1_thr, mode = "undirected",
+                                             weighted = TRUE)
+g_ET2_thr     <- graph_from_adjacency_matrix(m_ET2_thr, mode = "undirected",
+                                             weighted = TRUE)
+g_ET3_thr     <- graph_from_adjacency_matrix(m_ET3_thr, mode = "undirected",
+                                             weighted = TRUE)
+
+##¿COMO SE VE LA GRAFICA?, saque la visualizacion y otros atajos de esta pagina,
+#la dejo aqui para que le den una leida, es muy buena y para que lo metamos en
+#el documento de rmarkdown como cita
+
+plot(gh_obese_thr,
+     main = "HEALTHY & OBESE",
+     vertex.size = 5,
+     vertex.color = "purple",
+     vertex.label.cex = 0.05,
+     edge.color = "gray",
+     edge.width = 0.6,
+     layout = layout_with_fr)
+#Csárdi G, Nepusz T (2006). “The igraph software package for complex network research.” InterJournal, Complex Systems, 1695. https://igraph.org.
+#Ahi explican layout, tamaño de lineas, tamaño de texto, nodos, etc. 
+
+#---------------------------------------------------------------------
 #despues de eso continuar quitando los nodos aislados
+#tambien lo saque de un tutorial, dejo la pagina a continuacion por si la quieren
+#revisar, super funcional y para citar la pagina tambien.
+
+#Lo mas facil es hacer un ciclo que elimine los nodos y que vaya recorriendo cada objeto
+nodos_so<- list(
+  m_todos_thr,
+  mh_lean_thr,
+  mh_obese_thr,
+  mi_lean_thr,
+  mi_obese_thr,
+  m_danish_thr,
+  m_spanish_thr,
+  m_ET1_thr,
+  m_ET2_thr,
+  m_ET3_thr
+)
+
+titulos<- list(
+  "TODOS",
+  "Health & Lean",
+  "Health & Obese",
+  "IBD & Lean",
+  "IBD & Obese",
+  "Danish",
+  "Spanish",
+  "ET1",
+  "ET2",
+  "ET3"
+)
+
+red_filtrada<- list()
+
+for (i in 1:10){
+  #genero una variable temporal para que no se reescriba en los datos originales
+  #
+  v_temporal<- nodos_so[[i]]
+  g_original<- graph_from_adjacency_matrix(v_temporal, mode = "undirected",
+                                           weighted = TRUE)
+  g_filtrada<- delete.vertices(g_original, degree(g_original)== 0)
+  
+  #si solo corro el ciclo, no se van a guardar mis graficas, por eso, quiero que
+  #vaya guardando con el titulo 
+}
+
+
+
+
+
+
+
+
+
+delete.vertices()
+
+
+
+#https://colab.research.google.com/github/prbocca/na101_master/blob/master/homework_00_a_graphs/public_homework_00_a_graphs.ipynb
+
+
+#---------------------------------------------------------------------
 #para despuescalcular las metricas globales (# nodos, # edges, degree, distancias, coeficiente de clustering, modularidad)
 #después calcular centralidades
+
+
+
+
+
+
+
+
+
+
+
